d377536596f042703c16465419cda771529297dd
Jatin Bisen <jatinnbisen@gmail.com>
2025-03-31 23:33:18 +0530

    Update App.jsx
    titile edit

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -3,7 +3,7 @@ import Quiz from "./Quiz";
 function App() {
   return (
     <div className="app">
-      <h1>  🧠Newton Quiz Game</h1>
+      <h1>  🧠 Newton Quiz Game 🧠 </h1>
       <Quiz />
     </div>
   );
